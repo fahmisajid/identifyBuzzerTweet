1a90f89475ec99fa020abc5e71dd84ae7b897136
Muhamad Misykat Ali A <ahmad@sharingvision.com>
2023-03-01 08:37:05 +0700

    feat: update model using initial model

--- app.py
+++ app.py
@@ -3,28 +3,28 @@ import streamlit as st
 
 import pickle 
 
-st.set_page_config(page_title="App Name")
+st.set_page_config(page_title="Tuti Tweet Buzzer Detector v0.1")
 
-st.title("App Name")
-st.header("App Name")
+st.title("Tuti Tweet Buzzer Detector v0.1")
+st.header("Text classifier")
 
 #df = pd.read_csv('dummyhatespeech.csv')
-pkl_filename = "LR_Model.pkl"
+pkl_filename = "buzzermodel"
 
 #load model pickle
 with open(pkl_filename, 'rb') as file:
     classifier = pickle.load(file)
 
 #load TF Weight
-with open("countVectLR", 'rb') as file:
+with open("countvect", 'rb') as file:
     count_vect = pickle.load(file)
 
 #load IDF Weight
-with open("tfidfLR", 'rb') as file:
+with open("tfidf", 'rb') as file:
     tfidf_transformer = pickle.load(file)
 
 #input text
-sentence = st.text_input('Masukkan Kalimat:') 
+sentence = st.text_input('Masukkan teks:') 
 
 #transform/Extract (TF-IDF) new text 
 text_new =[sentence]
@@ -35,9 +35,12 @@ X_new_tfidf = tfidf_transformer.transform(X_new_counts)
 prediction = classifier.predict(X_new_tfidf)
 prediction_proba = classifier.predict_proba(X_new_tfidf)
 
+# 1 buzzer, 2 non buzzer
+label = "Buzzer" if prediction[0] == 1 else "Non Buzzer"
+label = f"{label} ({prediction_proba[0][1]})"
 if sentence:
     st.subheader("Hasil prediksi:")
-    st.write(prediction[0])
+    st.write(label)
           
           
           
