a8373f9025e9bcca13077f1255e139f786b54b2b
fahmisajid <45916595+fahmisajid@users.noreply.github.com>
2023-05-02 19:21:57 +0700

    Update accountbuzz.py

--- accountbuzz.py
+++ accountbuzz.py
@@ -149,10 +149,6 @@ with tab3:
                 if submit_button:
                     tweets_list2 = []
                     # Using TwitterSearchScraper to scrape data and append tweets to list
-                    """for i,tweet in enumerate(sntwitter.TwitterSearchScraper(search_term + ' since:'+since_date+' until:'+formatted_date).get_items()):
-                        if i>limit-1:
-                            break
-                        tweets_list2.append([tweet.content, tweet.user.username])"""
                     raise TypeError("Scrapping Lib still Error")    
                     # Creating a dataframe from the tweets list above
                     tweets_df2 = pd.DataFrame(tweets_list2, columns=['Text', 'Username'])
